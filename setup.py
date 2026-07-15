8b6040a7ca256b0007cc61adf18eb5fca3e6e11d
aleshazahirasiregar-oss <aleshazahirasiregar@gmail.com>
2026-07-15 23:05:47 +0700

    chore: update author and email to GarudaXyber official

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xysem_store",
+    version="1.0.0",
+    packages=find_packages(),
+    author="GarudaXyber",
+    author_email="aleshazahirasiregar@gmail.com",
+    description="Enterprise System Store Package by GarudaXyber",
+    license="MIT",
+)
